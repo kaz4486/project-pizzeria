d6ab3b795c22723db2d1562e873075afbdbfc074
Kamil <kaz4486@gmail.com>
2022-03-29 09:33:04 +0200

    Add processOrder function

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+/* global  utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
   ('use strict');
@@ -167,6 +167,38 @@
 
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData:', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          const optionSelected = formData[paramId]?.includes(optionId);
+          // check if the option is not default
+          if (!option.default && optionSelected) {
+            // add option price to price variable
+            price += option.price;
+            console.log(option.price + 'total:' + price);
+          } else if (option.default && !optionSelected) {
+            price -= option.price;
+            console.log(option.price + 'total:' + price);
+          }
+        }
+
+        // update calculated price in the HTML
+        thisProduct.priceElem.innerHTML = price;
+      }
     }
   }
 
